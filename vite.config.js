6f4605c24b14d14a6c379e900a84b40d416a5ea6
phgalal-create <ph.galal@gmail.com>
2026-05-08 13:31:18 +1000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { sveltekit } from '@sveltejs/vite-plugin-svelte';
 import { defineConfig } from 'vite';
 
+/** @type {import('vite').UserConfig} */
 export default defineConfig({
   plugins: [sveltekit()]
 });
